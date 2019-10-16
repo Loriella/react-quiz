6c294255505f8c6125c7da0898e943aebb08172a
Loriella <loris.orlova@gmail.com>
2019-10-16 16:31:20 +0300

    Working with server

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import classes from './QuizList.css';
 import {NavLink} from 'react-router-dom';
+import axios from 'axios';
 
 export default class QuizList extends Component {
 
@@ -15,6 +16,12 @@ export default class QuizList extends Component {
           </NavLink>
         </li>
       )
+    })
+  }
+
+  componentDidMount() {
+    axios.get('https://react-quiz-dfb7a.firebaseio.com/quiz.json').then(response => {
+      console.log(response)
     })
   }
 
