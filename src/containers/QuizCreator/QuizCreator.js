f895e31cdc6ef9b162a47fa42a7e3d8bf5b4fd9c
Loriella <loris.orlova@gmail.com>
2019-10-17 11:13:05 +0300

    Working with server Creating a test

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -5,6 +5,7 @@ import Input from "../../components/UI/Input/Input";
 import {createControl, validate, validateForm} from '../../form/formFramework';
 import Auxiliary from '../../hoc/Auxiliary/Auxiliary';
 import Select from "../../components/UI/Select/Select";
+import axios from 'axios';
 
 function createOptionControl(number) {
   return createControl({
@@ -71,10 +72,28 @@ export default class QuizCreator extends Component {
     })
   };
 
-  createQuizHandler = event => {
+  createQuizHandler = async event => {
     event.preventDefault();
 
-    console.log(this.state.quiz)
+    try {
+      await axios.post('https://react-quiz-dfb7a.firebaseio.com/quizes.json', this.state.quiz);
+
+      this.setState({
+        quiz: [],
+        isFormValid: false,
+        rightAnswerId: 1,
+        formControls: createFormControls()
+      })
+    } catch (e) {
+      console.log(e)
+    }
+
+    // axios.post('https://react-quiz-dfb7a.firebaseio.com/quizes.json', this.state.quiz)
+    //   .then(response => {
+    //     console.log(response)
+    //   })
+    //   .catch(error => console.log(error));
+
   };
 
   changeHandler = (value, controlName) => {
